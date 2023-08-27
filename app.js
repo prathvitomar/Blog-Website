593f594879466e6213decae2ef8942e4ccbdf6df
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2023-08-27 17:34:17 +0530

    Setup and Dependencies
    Added Express to project, then added few dependencies. and Added Route for different pages.

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000);
+
+app.get('/', (req, res)=>{
+    res.sendFile('./views/index.html', { root : __dirname })
+})
+
+app.get('/about', (req,res)=>{
+    res.sendFile('./views/about.html', { root : __dirname })
+})
+
+//Redirect to About 
+app.get('/about-us',(req, res)=>{
+    res.redirect('/about');
+})
+
+//Use function should be written at the last always.
+app.use((req, res)=>{
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+})
+
