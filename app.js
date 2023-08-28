1f9d2f13610dd7d16ad64dc8ed9d2338f1ee8c51
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2023-08-28 14:43:05 +0530

    Added MongoDB as Database and Mongoose
    Successfully added MongoDB and Mongoose. and Added Few Routes too

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
+const { result } = require('lodash');
+const Blog = require('./models/blog');
+// const username = encodeURIComponent("prathvi_tomar");
+// const password = encodeURIComponent("pt030499");
 const app = express();
-
+const dbURI = `mongodb+srv://prathvitomar030499:pt030499@blogdb.7xaamlp.mongodb.net/`
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result)=> app.listen(3000))
+    .catch((err)=> console.log(err));
 app.set('view engine','ejs');
 
-app.listen(3000);
+// app.listen(3000);
 
 app.use(express.static('public'))
 app.use(morgan('dev'))
 
+
 app.get('/', (req, res)=>{
-    const blogs = [
-        { title: 'Jujutsu Kaisen', snippet : 'New Gen Shounen Gem'},
-        { title: 'Sakamoto Days', snippet : 'Most Entertaining Manga right now'}
-    ]
-    res.render('index', { title : 'Home', blogs});
+    res.redirect('/blogs');
+})
+
+app.get('/blogs', (req, res)=>{
+    Blog.find().sort({ createdAt: -1})
+    .then((result)=>{
+        res.render('index', { title: 'All Blogs', blogs : result})
+    }).catch((err)=>{
+        console.log(err)
+    })
 })
 
 app.get('/about', (req,res)=>{
