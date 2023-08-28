6a1e29155a05899042a09b26f0cbdbd6ff9c9b61
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2023-08-28 21:50:38 +0530

    Added Express Routes and Controllers
    Made Controller functions for different Routes and Added Express Route to make Code cleaner.

--- app.js
+++ app.js
@@ -1,47 +1,43 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const { result } = require('lodash');
-const Blog = require('./models/blog');
-// const username = encodeURIComponent("prathvi_tomar");
-// const password = encodeURIComponent("pt030499");
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
 const app = express();
-const dbURI = `mongodb+srv://prathvitomar030499:pt030499@blogdb.7xaamlp.mongodb.net/`
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result)=> app.listen(3000))
-    .catch((err)=> console.log(err));
-app.set('view engine','ejs');
 
-// app.listen(3000);
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://prathvitomar030499:pt030499@blogdb.7xaamlp.mongodb.net/";
 
-app.use(express.static('public'))
-app.use(morgan('dev'))
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
+// register view engine
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
-    res.redirect('/blogs');
-})
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-app.get('/blogs', (req, res)=>{
-    Blog.find().sort({ createdAt: -1})
-    .then((result)=>{
-        res.render('index', { title: 'All Blogs', blogs : result})
-    }).catch((err)=>{
-        console.log(err)
-    })
-})
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
 
-app.get('/about', (req,res)=>{
-    res.render('about', { title : 'About'});
-})
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
-//Redirect to About 
-app.get('/blogs/create',(req, res)=>{
-    res.render('create', { title : 'Create'});
-})
+// blog routes
+app.use('/blogs', blogRoutes);
 
-//Use function should be written at the last always.
-app.use((req, res)=>{
-    res.status(404).render('404', { title : '404'});
-})
-
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
